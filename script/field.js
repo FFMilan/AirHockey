ea39dacf7a3972669cae3dc254b15d90dac99f0e
nicokant94 <niccolocantu94@hotmail.it>
2015-05-22 17:05:58 +0200

    Populating...

---
+++ script/field.js
@@ -0,0 +1,51 @@
+function Field() {
+	var x = window.innerWidth;
+	var y = window.innerHeight;
+	var elements = [];
+
+	this.getWidth = function() {
+		return x;
+	}
+
+	this.getHeight = function() {
+		return y;
+	}
+
+	this.getElements = function() {
+		return elements;
+	}
+
+	this.addElement = function(elem) {
+		elements.push(elem);
+	}
+
+	this.collidesBounds = function(elem) {
+		var radius = elem.getPosition().radius;
+		var posX = elem.getPosition().x;
+		var posY = elem.getPosition().y;
+		var bounds = [0,y,0,x];
+		for (bound of bounds) {
+			var distance = posX + posY + bound;
+			if(distance <= radius) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	this.collidesWith = function(elem1, elem2) {
+		var radius1 = elem1.getPosition().radius;
+		var radius2 = elem2.getPosition().radius;
+		var posX1 = elem1.getPosition().x;
+		var posY1 = elem1.getPosition().y;
+		var posX2 = elem2.getPosition().x;
+		var posY2 = elem2.getPosition().x;
+		var distance = Math.sqrt(Math.pow(posX1 + posX2, 2) + Math.pow(posY1 + posY2, 2));
+		if (distance - (radius1 + radius2) <= 0)
+			return true;
+		else
+			return false;
+	}
+
+	return this;
+}
