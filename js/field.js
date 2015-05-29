6529103897f89bb308a2745ceb9a9ab227e9f870
Aronne Brivio <aroblu94@gmail.com>
2015-05-29 15:58:55 +0200

    Working on physics... preparing the environment...
    Fixed indentation, testing power

--- js/field.js
+++ js/field.js
@@ -19,23 +19,31 @@ function Field() {
 		elements.push(elem);
 	}
 
+	/* Check elem-bounds collisions 
+	 *
+	 * Idea: an element collide with bounds only in
+	 * 4 positions (N,S,E,W). So we're checking
+	 * the element doesn't collide with field bounds
+	 * in this pisitions.
+	 */
 	this.collidesBounds = function(elem) {
 		var radius = elem.getPosition().radius;
 		var posX = elem.getPosition().x;
 		var posY = elem.getPosition().y;
 		var bounds = [0,y,0,x];
-		if((posX > 0+radius && posX < x-radius) && (posY > 0+radius && posY < y-radius))
-		/*for (bound of bounds) {
+		//if((posX > 0+radius && posX < x-radius) && (posY > 0+radius && posY < y-radius))
+		for (bound of bounds) {
 			var distance = posX + posY + bound;
 			if(distance <= radius) {
 				return true;
 			}
-		}*/
-			return false;
-		else
-			return true;
+		}
+		return false;
+		/*else
+			return true;*/
 	}
 
+	/* Check 2-elems collision using distance between elems */
 	this.collidesWith = function(elem1, elem2) {
 		var radius1 = elem1.getPosition().radius;
 		var radius2 = elem2.getPosition().radius;
--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ var pad = new Pad();
 
 
 window.onload = function() {
-  console.log("here");
+	console.log("here");
 	var canvas = document.createElement("canvas");
 	document.body.appendChild(canvas);
 	var ctx = canvas.getContext("2d");
@@ -15,43 +15,41 @@ window.onload = function() {
 	
 	var lastPress = null;
 	var direction = null;
-  var power = 100;
+	//var power = 100;
 
-  var incrementer = null;
+	//var incrementer = null;
 	canvas.addEventListener("mousedown",function(e){
-    //check if the pad is pressed
+		//check if the pad is pressed
+		if(isOver({x : e.clientX, y: e.clientY},
+			{x : pad.getPosition().x ,
+			x2 : pad.getPosition().x + pad.getPosition().dims,
+			y : pad.getPosition().y,
+			y2 : pad.getPosition().y + pad.getPosition().dims}
+			)){
+			lastPress = {x : pad.getCenterPosition().x, y: pad.getCenterPosition().y};
+			//incrementer = setInterval(function(){power -= 10},120);
+		} else {
+			lastPress = null;
+			direction = null;
+		}
+	});
 
-    if(isOver({x : e.clientX, y: e.clientY},
-      {x : pad.getPosition().x ,
-       x2 : pad.getPosition().x + pad.getPosition().dims,
-       y : pad.getPosition().y,
-       y2 : pad.getPosition().y + pad.getPosition().dims}
-     )){
-      lastPress = {x : pad.getCenterPosition().x, y: pad.getCenterPosition().y};
-      incrementer = setInterval(function(){power -= 10},120);
-    }else{
-      lastPress = null;
-      direction = null;
-    }
-  });
+	var distance = 0;
+	var angle = 0;
 
-  var distance = 0;
-  var angle = 0;
-  
-  canvas.addEventListener("mouseup",function(e){
-
-    if(lastPress){
-      clearInterval(incrementer);
-      direction = (e.clientX > lastPress.x)? true : false;
-      distance = Math.sqrt(Math.pow(e.clientX - lastPress.x, 2) + Math.pow(e.clientY - lastPress.y, 2), 2);
-      angle = Math.atan((e.clientY - lastPress.y) / (e.clientX - lastPress.x));
-      power = (power < 0) ? 0 : power;
-      pad.animateUntil(angle,distance,direction,ctx,power);
-      power = 100;
-    }
-  });
+	canvas.addEventListener("mouseup",function(e){
+		if(lastPress){
+			//clearInterval(incrementer);
+			direction = (e.clientX > lastPress.x)? true : false;
+			distance = Math.sqrt(Math.pow(e.clientX - lastPress.x, 2) + Math.pow(e.clientY - lastPress.y, 2), 2);
+			angle = Math.atan((e.clientY - lastPress.y) / (e.clientX - lastPress.x));
+			//power = (power < 0) ? 0 : power;
+			pad.animateUntil(angle,distance,direction,ctx);//,power);
+			//power = 100;
+		}
+	});
 }
 
 function isOver(point,elem){
-  return (point.x > elem.x && point.x < elem.x2) && (point.y > elem.y && point.y < elem.y2)
+	return (point.x > elem.x && point.x < elem.x2) && (point.y > elem.y && point.y < elem.y2)
 }
