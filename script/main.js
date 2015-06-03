77709b8400c90eb9865c359c903b7026ef63c436
Aronne Brivio <aroblu94@gmail.com>
2015-06-03 18:54:59 +0200

    Trying to block the pad while dragging it

--- script/main.js
+++ script/main.js
@@ -1,19 +1,21 @@
 window.onload = function() {
 	var canvas = document.createElement("canvas");
+	document.body.appendChild(canvas);
+	canvas.id = "my_canvas";
 	var ctx = canvas.getContext("2d");
 	var ratio = window.innerHeight / 7;
-	canvas.height = window.innerHeight;
-	canvas.width = ratio * 15;
+	document.body.style.height = window.innerHeight;
+	document.body.style.width = ratio * 15;
 
 	var world = Physics();
-	var renderer = Physics.renderer(canvas, {
+	var renderer = Physics.renderer('canvas', {
+		el: "my_canvas",
 		meta: false,
 		styles: {
 			'circle' : {
 				strokeStyle: 'hsla(60, 37%, 17%, 1)',
 				lineWidth: 1,
-				fillStyle: 'hsla(60, 37%, 57%, 0.8)',
-				angleIndicator: 'hsla(60, 37%, 17%, 0.4)'
+				fillStyle: 'hsla(60, 37%, 57%, 0.8)'
 			}
 		}
 	});
@@ -21,14 +23,40 @@ window.onload = function() {
 	world.on('step', function(){
 		world.render();
 	});
+
 	var pad = Physics.body("circle", {
 		x: 50,
 		y: 50,
-		radius: 20
+		radius: ratio/2,
+		treatment: "dynamic",
+		mass: 1.0,
+		restitution: 0.8,
+		cof: 0.8
 	});
 	world.add(pad);
-	/*var field = new Field();
-	var pad = new Pad();*/
+
+	var bounds = Physics.aabb(0, 0, ratio * 15, window.innerHeight);
+	world.add( Physics.behavior('edge-collision-detection', {
+		aabb: bounds,
+		restitution: 0.3
+	}));
+	world.add(Physics.behavior('body-impulse-response'));
+	world.add(Physics.behavior('interactive', {
+		el: "my_canvas"
+	}));
+
+	world.on("interact:grab", function(e) {
+		if(e.body) {
+			console.log(e.body);
+			e.body.treatment = 'static';
+		}
+	});
+	world.on("interact:release", function(e) {
+		if(e.body)
+			e.body.treatment = "dynamic";
+	});
+
+
 
 	Physics.util.ticker.on(function(time, dt){
 		world.step(time);
